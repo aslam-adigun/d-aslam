6c5b0e3f8d36d012992625eefc874a6bdd0d0847
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2023-09-21 02:29:10 +0100

    :rocket: Completed Experience & Portfolio sections
    I created the experience & portfolio sections, added styling and designed for responsiveness.

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,131 @@
 import React from 'react'
 import './experience.css'
+import {BsCheckLg} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Where I've worked</h5>
+      <h2>Experience</h2>
+
+      <div className="container experiences__container">
+        <article className="experience">
+          <div className="experience__head">
+            <h3>Design Lead</h3>
+            <p>Techstars Community</p>
+            <p className='date'>Mar 2023 till date</p>
+          </div>
+
+          <ul className="experience__list">
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I created Techstars Community Brand Visual Identity.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>Currently designing on Bookslab, an app being developed by the community.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF DESIGN LEAD */}
+        <article className="experience">
+          <div className="experience__head">
+            <h3>IT Support Specialist</h3>
+            <p>Emporg Limited</p>
+            <p className='date'>Feb 2021 till date</p>
+          </div>
+
+          <ul className="experience__list">
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I provide personalized technical support to users.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I troubleshoot hardware and software issues.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I assist with installation and configuration, and maintaining documentation.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I also contribute to network and system maintenance.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF IT SUPPORT */}
+        <article className="experience">
+          <div className="experience__head">
+            <h3>Graphics Designer</h3>
+            <p>Alvoice Solutions</p>
+            <p className='date'>May 2018 till date</p>
+          </div>
+
+          <ul className="experience__list">
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I created captivating visual concepts that effectively drove company sales.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I developed the overall layout and production design for diverse applications.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I adjusted layouts based on valuable feedback from clients and team members.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I presented designs to clients for approval or modification.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF GRAPHICS DESIGN */}
+        <article className="experience">
+          <div className="experience__head">
+            <h3>Graphics Designer</h3>
+            <p>WeTheEcho</p>
+            <p className='date'>Mar 2021 - May 2022</p>
+          </div>
+
+          <ul className="experience__list">
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I created captivating visual concepts that effectively drove company sales.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I adjusted layouts based on valuable feedback from clients and team members.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I presented designs to clients for approval or modification.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF GRAPHICS DESIGN */}
+        <article className="experience">
+          <div className="experience__head">
+            <h3>Graphics Designer</h3>
+            <p>Sorai Ventures</p>
+            <p className='date'>January 2018 - Sep 2018</p>
+          </div>
+
+          <ul className="experience__list">
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I developed captivating visuals for diverse applications.</p>
+            </li>
+            <li>
+              <BsCheckLg className="experience__list-icon" />
+              <p>I determined the optimal medium, adjusted layouts based on feedback.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF GRAPHICS DESIGN */}
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project 1',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project 2',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project 3',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project 4',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Project 5',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Project 6',
+    figma: 'https://www.figma.com/@d_aslam',
+    demo: 'https://www.figma.com/@d_aslam'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, figma, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+          <a href={figma} className='btn' target='_blank'>Figma</a>
+          <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
